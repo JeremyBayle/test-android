c4b4dd74942b827108f35aad3540007d5f6b7f75
Jérémy Bayle <jeremy.bayle@drivequant.com>
2021-11-27 11:21:15 +0100

    Update user list with picture

--- app/src/main/java/com/baylej/android/test/ui/userslist/UsersListAdapter.kt
+++ app/src/main/java/com/baylej/android/test/ui/userslist/UsersListAdapter.kt
@@ -8,11 +8,17 @@ import com.baylej.android.core.model.User
 import android.content.Context
 
 import android.view.LayoutInflater
+import android.widget.ImageView
 import android.widget.TextView
 import com.baylej.android.test.R
+import com.baylej.android.test.ui.utils.CircleTransformation
+import com.squareup.picasso.Picasso
+import java.util.*
 
 
 class UsersListAdapter(private val context: Context, private var users: List<Pair<Char, List<User>>>) : BaseExpandableListAdapter() {
+
+    private val circleTransformation = CircleTransformation()
 
     fun updateUsers(users: List<Pair<Char, List<User>>>) {
         this.users = users
@@ -56,8 +62,22 @@ class UsersListAdapter(private val context: Context, private var users: List<Pai
         } else {
             convertView
         }
-        val userLastNameTextView = view.findViewById<TextView>(R.id.user_last_name)
-        userLastNameTextView.text = user.lastName
+        val userLastNameTextView = view.findViewById<TextView>(R.id.user_name)
+        val pictureView = view.findViewById<ImageView>(R.id.user_picture)
+        val name = user.title + " " + user.firstName + " " + user.lastName
+        userLastNameTextView.text = name.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
+        }
+        Picasso.get()
+            .load(user.picture)
+            .placeholder(R.drawable.ic_user_placeholder)
+            .error(R.drawable.ic_user_placeholder)
+            .fit()
+            .centerCrop()
+            .transform(circleTransformation)
+            .into(pictureView)
         return view
     }
 
