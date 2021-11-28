3b89c0322224ea88cf2cc9555e1c6786adb46f7b
Jérémy Bayle <jeremy.bayle@drivequant.com>
2021-11-28 11:03:55 +0100

    Add UserDetailRepository unit tests

---
+++ data/src/test/java/com/baylej/android/data/repository/UserDetailRepositoryUT.kt
@@ -0,0 +1,66 @@
+package com.baylej.android.data.repository
+
+import com.baylej.android.core.model.ResultWrapper
+import com.baylej.android.core.repository.UserDetailsRepository
+import com.baylej.android.core.repository.UserRepository
+import com.baylej.android.data.base.BaseUT
+import com.baylej.android.data.di.apiModulesTest
+import com.baylej.android.data.di.mockWebServerModuleTest
+import com.baylej.android.data.di.repositoryModules
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.koin.core.context.startKoin
+import org.koin.test.inject
+import java.net.HttpURLConnection
+
+@RunWith(JUnit4::class)
+class UserDetailRepositoryUT: BaseUT() {
+
+    private val userDetailRepository: UserDetailsRepository by inject()
+    private val userId = "60d0fe4f5311236168a109ca"
+
+    @Before
+    fun start(){
+        super.setUp()
+        startKoin{ modules(
+            mockWebServerModuleTest +
+                    apiModulesTest(getMockWebServerUrl()) +
+                    repositoryModules
+        )}
+    }
+
+    @Test
+    fun `test user repository success response`()  =  runBlocking {
+        mockNetworkResponseWithFileContent("success_response_detail.json", HttpURLConnection.HTTP_OK)
+        val repoData = userDetailRepository.getUserDetails(userId)
+
+        Assert.assertNotNull(repoData)
+        Assert.assertEquals(true, repoData is ResultWrapper.Success)
+        Assert.assertEquals("sara.andersen@example.com", (repoData as ResultWrapper.Success).value.email)
+        Assert.assertEquals("92694011", repoData.value.phone)
+        Assert.assertEquals("Denmark", repoData.value.location.country)
+    }
+
+    @Test
+    fun `test user repository no connection error`()  =  runBlocking {
+        stopMockServer()
+        val repoData = userDetailRepository.getUserDetails(userId)
+
+        Assert.assertNotNull(repoData)
+        Assert.assertEquals(true, repoData is ResultWrapper.NetworkError)
+    }
+
+    @Test
+    fun `test user repository http error`()  =  runBlocking {
+        mockNetworkResponseWithFileContent("error_invalid_app_id.json", HttpURLConnection.HTTP_FORBIDDEN)
+        val repoData = userDetailRepository.getUserDetails(userId)
+
+        Assert.assertNotNull(repoData)
+        Assert.assertEquals(true, repoData is ResultWrapper.ErrorResponse)
+        Assert.assertEquals(403, (repoData as ResultWrapper.ErrorResponse).code)
+    }
+}
